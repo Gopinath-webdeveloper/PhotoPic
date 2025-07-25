6eb986f29abc15a64fedf7336dd8498fb6b5e447
Gopinath-webdeveloper <gopinath30127@gmail.com>
2025-07-25 21:05:05 +0530

    file chages

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
-  port: process.env.PORT ,
+  port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || 'development',
   apiBaseUrl: process.env.API_BASE_URL,
   corsOrigin: process.env.CORS_ORIGIN,
