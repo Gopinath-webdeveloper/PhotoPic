c714b00699a722f5376acde401e81521ff11e33c
Gopinath-webdeveloper <gopinath30127@gmail.com>
2025-07-26 15:09:58 +0530

    roles changed

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['Admin', 'Moderator', 'Member'], default: 'Member' },
+  role: { type: String, enum: ['Admin', 'Moderator', 'Member'], default: 'Admin' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema); 
